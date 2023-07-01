429db9acb4dd6cf452f671a80511ccb583459c63
Angmtlln <136786363+Angmtlln@users.noreply.github.com>
2023-07-01 21:03:36 +0300

    Update train.py

--- train.py
+++ train.py
@@ -1,10 +1,10 @@
-
+# Import necessary libraries
 from math import radians, sin, cos
 from nn import Net
 from random import randint as rnd, random
 
-
-def angle_to_array(angle): # Function to convert an angle into an RGB color array
+# Function to convert angle to RGB array representation
+def angle_to_array(angle):
     if 0 <= angle < 60:
         return (1 - angle / 60, angle / 60, 0)
     if 60 <= angle < 120:
@@ -14,35 +14,78 @@ def angle_to_array(angle): # Function to convert an angle into an RGB color arra
         angle -= 120
         return (angle / 60, 0, 1 - angle / 60)
 
-
-def angle_to_line(angle): # Function to convert an angle into a line equation in the form of ax + by + c = 0
+# Function to convert angle to line representation
+def angle_to_line(angle):
     angle = radians(angle)
     x1, y1 = cos(angle), sin(angle)
     x2, y2 = -x1, -y1
     b = x2 - x1
     a = y1 - y2
-    c = -(a * x1 + b * y1) + (random() - 0.5) * 6
+    c = -(a * x1 + b * y1)
     return a, b, c
 
-
-net = Net(7 * 7 * 3, 50, 3)
+# Create an instance of the neural network class with the specified dimensions
+net = Net(7 * 7 * 3, 50, 4)
 
 q = 0
 while True:
     col1 = (rnd(0, 255), rnd(0, 255), rnd(0, 255))
-    col2 = col1
-    while sum(abs(i - j) for i, j in zip(col1, col2)) < 60:
-        col2 = (rnd(0, 255), rnd(0, 255), rnd(0, 255))
-    angle = random() * 180
-    a, b, c = angle_to_line(angle) # Convert the angle to a line equation
     test = []
-    for x in range(-3, 4):
-        for y in range(-3, 4):
-            # Determine the color based on the line equation and colors col1 and col2
-            col = [min(max(i + rnd(-20, 20), 0), 255) for i in (col1, col2)[a * x + b * y + c > 0]]
-            test += [i / 255 for i in col]
-    net.train(test, angle_to_array(angle), 0.5)
+    
+    # Generate test input and target output for training
+    if rnd(0, 6):
+        col2 = col1
+        while sum(abs(i - j) for i, j in zip(col1, col2)) < 50:
+            col2 = (rnd(0, 255), rnd(0, 255), rnd(0, 255))
+        angle = random() * 180
+        a, b, c = angle_to_line(angle)
+        
+        # Randomly adjust line position
+        if rnd(0, 2):
+            c += (random() - 0.5) * 6
+            for x in range(-3, 4):
+                for y in range(-3, 4):
+                    col = [min(max(i + rnd(-20, 20), 0), 255) for i in (col1, col2)[a * x + b * y + c > 0]]
+                    test += [i / 255 for i in col]
+        else:
+            c += (random() - 0.5) * 3
+            C = c
+            c += 0.5 + random() * 2
+            C -= 0.5 + random() * 2
+            col3 = col1
+            
+            # Generate a third color if needed
+            if not rnd(0, 3):
+                while any(sum(abs(i - j) for i, j in zip(col3, cl)) < 50 for cl in (col1, col2)):
+                    col3 = (rnd(0, 255), rnd(0, 255), rnd(0, 255))
+                    
+            for x in range(-3, 4):
+                for y in range(-3, 4):
+                    col = col2
+                    axby = a * x + b * y
+                    
+                    # Assign color based on line position
+                    if axby + c < 0 and axby + C < 0: 
+                        col = col1
+                    if axby + c > 0 and axby + C > 0: 
+                        col = col3
+                    col = [min(max(i + rnd(-20, 20), 0), 255) for i in col]
+                    test += [i / 255 for i in col]
+                    
+        # Train the neural network
+        net.train(test, angle_to_array(angle) + (0,), 0.5)
+    else:
+        for x in range(-3, 4):
+            for y in range(-3, 4):
+                col = [min(max(i + rnd(-30, 30), 0), 255) for i in col1]
+                test += [i / 255 for i in col]
+        angle = list(net.get(test))
+        angle[-1] = 1
+        net.train(test, angle, 0.5)
+    
     q += 1
+    
+    # Save weights every 5000 iterations
     if q % 5000 == 0:
-        net.save(f'weights/art{q // 5000}') # Save the weights of the neural network to a file
+        net.save(f'weights/art{q // 5000}')
         print(q)
